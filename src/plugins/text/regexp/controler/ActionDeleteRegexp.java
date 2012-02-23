06d1469ebdca2f34c728b00cc8f7b438a3c9a02b
Christophe Cerisara <cerisara@loria.fr>
2012-02-23 11:03:00 +0100

    encoding bugs

--- src/plugins/text/regexp/controler/ActionDeleteRegexp.java
+++ src/plugins/text/regexp/controler/ActionDeleteRegexp.java
@@ -10,34 +10,34 @@ Copyright Christophe Cerisara, Josselin Pierre (1er septembre 2008)
 
 cerisara@loria.fr
 
-Ce logiciel est un programme informatique servant à aligner un
+Ce logiciel est un programme informatique servant e aligner un
 corpus de parole avec sa transcription textuelle.
 
-Ce logiciel est régi par la licence CeCILL-C soumise au droit français et
+Ce logiciel est regi par la licence CeCILL-C soumise au droit franeais et
 respectant les principes de diffusion des logiciels libres. Vous pouvez
 utiliser, modifier et/ou redistribuer ce programme sous les conditions
-de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA 
+de la licence CeCILL-C telle que diffusee par le CEA, le CNRS et l'INRIA 
 sur le site "http://www.cecill.info".
 
-En contrepartie de l'accessibilité au code source et des droits de copie,
-de modification et de redistribution accordés par cette licence, il n'est
-offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
-seule une responsabilité restreinte pèse sur l'auteur du programme,  le
-titulaire des droits patrimoniaux et les concédants successifs.
+En contrepartie de l'accessibilite au code source et des droits de copie,
+de modification et de redistribution accordes par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitee.  Pour les memes raisons,
+seule une responsabilite restreinte pese sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concedants successifs.
 
-A cet égard  l'attention de l'utilisateur est attirée sur les risques
-associés au chargement,  à l'utilisation,  à la modification et/ou au
-développement et à la reproduction du logiciel par l'utilisateur étant 
-donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
-manipuler et qui le réserve donc à des développeurs et des professionnels
-avertis possédant  des  connaissances  informatiques approfondies.  Les
-utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
-logiciel à leurs besoins dans des conditions permettant d'assurer la
-sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
-à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+A cet egard  l'attention de l'utilisateur est attiree sur les risques
+associes au chargement,  e l'utilisation,  e la modification et/ou au
+developpement et e la reproduction du logiciel par l'utilisateur etant 
+donne sa specificite de logiciel libre, qui peut le rendre complexe e 
+manipuler et qui le reserve donc e des developpeurs et des professionnels
+avertis possedant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invites e charger  et  tester  l'adequation  du
+logiciel e leurs besoins dans des conditions permettant d'assurer la
+securite de leurs systemes et ou de leurs donnees et, plus generalement, 
+e l'utiliser et l'exploiter dans les memes conditions de securite. 
 
-Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
-pris connaissance de la licence CeCILL-C, et que vous en avez accepté les
+Le fait que vous puissiez acceder e cet en-tete signifie que vous avez 
+pris connaissance de la licence CeCILL-C, et que vous en avez accepte les
 termes.
 */
 
@@ -51,7 +51,7 @@ import javax.swing.JList;
 import plugins.text.regexp.TypeElement;
 
 /**
- * ActionListener associé aux boutons delete regexp from list
+ * ActionListener associe aux boutons delete regexp from list
  */
 public class ActionDeleteRegexp implements ActionListener {
 	//------ Private Fields --------
@@ -64,7 +64,7 @@ public class ActionDeleteRegexp implements ActionListener {
 		this.liste = liste;
 	}
 
-	//------- Action associée ------
+	//------- Action associee ------
 	public void actionPerformed(ActionEvent e){
 		int indice = liste.getSelectedIndex();
 		if(indice != -1){
