550eeabae339a95417036fb2cda511323eb3518b
Christophe Cerisara <cerisara@loria.fr>
2012-06-22 22:21:22 +0200

    ok

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 jar cvf jtransres.zip res libs ressources *.txt culture.wav
 scp jtransres.zip talc1:/var/www/users/cerisara/jtrans/
+scp jtrans.jar talc1:/var/www/users/cerisara/jtrans/
+scp docs/index.html talc1:/var/www/users/cerisara/jtrans/
 
 # zip -r -9 jtransres.zip res/ libs/ ressources/ *.txt culture.wav
 
