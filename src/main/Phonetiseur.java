657b7043a01ba0be7ffe1853580745f4ade9e939
xtof <xtof@biber.(none)>
2012-12-02 10:47:55 +0100

    phonetiseur acces commandline

--- src/main/Phonetiseur.java
+++ src/main/Phonetiseur.java
@@ -1,10 +1,28 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import phonetiseur.FileUtils;
 import plugins.phonetiseur.PhonetiseurFacade;
+import plugins.speechreco.grammaire.Grammatiseur;
 
 
 public class Phonetiseur {
 	private PhonetiseurFacade ph = null;
+	
+	public static void main(String[] args) throws Exception {
+		Grammatiseur gram = Grammatiseur.getGrammatiseur();
+		BufferedReader f= new FileUtils().openFileUTF(args[0]);
+		for (int i=0;;i++) {
+			String s=f.readLine();
+			if (s==null) break;
+			String r = gram.getGrammar(s);
+			System.out.println(s+" "+r);
+		}
+		f.close();
+	}
+	
 	
 	public String phonetise(String mot) {
 //		if (ph==null) ph = new PhonetiseurFacade(cheminFichierMatriceProbaEtLexique, cheminRepArff, cheminRepModel);
