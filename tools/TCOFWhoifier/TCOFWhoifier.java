b2dfe7920694237c84ea5d5da10114faca34df5a
Iliyas Jorio <inv.jorio@gmail.com>
2014-02-12 11:55:20 +0100

    TCOFWhoifier

---
+++ tools/TCOFWhoifier/TCOFWhoifier.java
@@ -0,0 +1,219 @@
+import org.w3c.dom.*;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Adds Who tags to Transcriber files that use the TCOF convention for
+ * overlapping speech.
+ *
+ * The TCOF corpus does not use Who tags, which is the standard Transcriber way
+ * of denoting overlapping speech. Instead, TCOF denotes overlaps with angle
+ * brackets across two Turns. In the following example, the words "four five
+ * six" and "seven eight nine" are spoken simultaneously.
+ *
+ * <pre>
+ * {@code
+ * <Turn speaker="spk1" ...>
+ *     <Sync time="1.234"/> one two three &lt; four five six
+ * </Turn>
+ *
+ * <Turn speaker="spk2" ...>
+ *     <Sync time="5.678"/> seven eight nine &gt; ten eleven twelve
+ * </Turn>
+ * }</pre>
+ *
+ * This tool would transform the example above into multi-speaker Turns, which,
+ * as far as JTrans is concerned, is more manageable Transcriber markup.
+ *
+ * <pre>
+ * {@code
+ * <Turn speaker="spk1" ...>
+ *     <Sync time="1.234"/> one two three
+ * </Turn>
+ *
+ * <Turn speaker="spk1 spk2" ...>
+ *     <Sync time="5.678"/>
+ *     <Who nb="1"/> &lt; four five six
+ *     <Who nb="2"/> seven eight nine &gt; ten eleven twelve
+ * </Turn>
+ * }</pre>
+ */
+public class TCOFWhoifier {
+
+	public static Pattern DTD_PATTERN = Pattern.compile("^.*(trans-[0-9a-z]*\\.dtd)$");
+
+
+	private static List<Node> scrap(Node node) {
+		List<Node> scrapped = new ArrayList<Node>();
+		while (null != node) {
+			scrapped.add(node);
+			Node n = node;
+			node = n.getNextSibling();
+			n.getParentNode().removeChild(n);
+		}
+		return scrapped;
+	}
+
+
+	public static Document whoify(File file)
+			throws IOException, ParserConfigurationException, SAXException
+	{
+		Document doc = newXMLDocumentBuilder().parse(file);
+
+		String previousSpeaker = null;
+		Element reportScrapped = null;
+		List<Node> reportOverlap = null;
+
+		NodeList turnList = doc.getElementsByTagName("Turn");
+		for (int turnNo = 0; turnNo < turnList.getLength(); turnNo++) {
+			Element turn = (Element)turnList.item(turnNo);
+
+			boolean hangingOverlap = reportOverlap != null;
+			final boolean multiSpeakerTurn = hangingOverlap;
+
+			String singleSpeaker = turn.getAttribute("speaker");
+			if (singleSpeaker.isEmpty() || 1 != singleSpeaker.split(" ").length)
+				throw new Error("speaker count != 1. If >0, already whoified");
+
+			assert reportOverlap == null || reportScrapped == null;
+
+			if (reportOverlap != null) {
+				turn.setAttribute("speaker", previousSpeaker + " " + singleSpeaker);
+			}
+
+			if (reportScrapped != null) {
+				if (singleSpeaker.equals(previousSpeaker)) {
+					NodeList rscn = reportScrapped.getChildNodes();
+					for (int j = rscn.getLength()-1; j >= 0; j--) {
+						System.out.println("   " + rscn.item(j).getTextContent());
+						turn.insertBefore(rscn.item(j), turn.getFirstChild());
+					}
+					turn.setAttribute("startTime", reportScrapped.getAttribute("startTime"));
+				} else {
+					turn = (Element)turn.getParentNode().insertBefore(reportScrapped, turn);
+					singleSpeaker = turn.getAttribute("speaker");
+				}
+				reportScrapped = null;
+			}
+
+			// We may have changed our turn
+
+			Node child = turn.getFirstChild();
+			previousSpeaker = singleSpeaker;
+
+			while (null != child) {
+				String name = child.getNodeName();
+				String text = child.getTextContent();
+
+				if (name.equals("Sync") && reportOverlap != null) {
+					Node nextChild = child.getNextSibling();
+
+					Element who1 = doc.createElement("Who");
+					who1.setAttribute("nb", ""+1);
+					Element who2 = doc.createElement("Who");
+					who2.setAttribute("nb", ""+2);
+
+					turn.insertBefore(who1, nextChild);
+					for (Node n: reportOverlap)
+						turn.insertBefore(n, nextChild);
+					turn.insertBefore(who2, nextChild);
+
+					reportOverlap = null;
+					hangingOverlap = false;
+					child = nextChild;
+				}
+
+				else if (name.equals("Sync") && reportOverlap == null && multiSpeakerTurn) {
+					String syncTime =  ((Element)child).getAttribute("time");
+
+					reportScrapped = doc.createElement("Turn");
+					reportScrapped.setAttribute("endTime", turn.getAttribute("endTime"));
+					reportScrapped.setAttribute("startTime", syncTime);
+					reportScrapped.setAttribute("speaker",   singleSpeaker);
+					for (Node n: scrap(child))
+						reportScrapped.appendChild(n);
+
+					turn.setAttribute("endTime", syncTime);
+					break;
+				}
+
+				else if (name.equals("#text") && text.contains("<")) {
+					if (reportOverlap != null)
+						throw new Error("overlap already ongoing before '" + text + "'");
+
+					int chevronPos = text.indexOf('<');
+					String beforeChevron = text.substring(0, chevronPos);
+					child.setTextContent(beforeChevron);
+
+					reportOverlap = scrap(child.getNextSibling());
+					reportOverlap.add(0, doc.createTextNode(text.substring(chevronPos)));
+					break;
+				}
+
+				else {
+					child = child.getNextSibling();
+				}
+			}
+
+			if (hangingOverlap)
+				throw new Error("hanging overlap - missing Sync tag?");
+		}
+
+		return doc;
+	}
+
+
+	/**
+	 * Return a DocumentBuilder suitable to parsing a TRS file.
+	 */
+	private static DocumentBuilder newXMLDocumentBuilder() throws ParserConfigurationException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setValidating(true);
+		dbf.setNamespaceAware(true);
+		DocumentBuilder builder = dbf.newDocumentBuilder();
+
+		builder.setEntityResolver(new EntityResolver() {
+			@Override
+			public InputSource resolveEntity(String publicId, String systemId) throws IOException {
+				Matcher m = DTD_PATTERN.matcher(systemId);
+				if (m.matches()) {
+					return new InputSource(new FileInputStream(m.group(1)));
+				} else {
+					return null;
+				}
+			}
+		});
+
+		return builder;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.out.println("USAGE:\n" +
+					"TCOFWhoifier <INPUT.trs> <OUTPUT.trs>");
+			System.exit(1);
+		}
+
+		Document doc = whoify(new File(args[0]));
+
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		transformer.setOutputProperty(OutputKeys.ENCODING, doc.getXmlEncoding());
+		transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, doc.getDoctype().getSystemId());
+
+		transformer.transform(
+				new DOMSource(doc),
+				new StreamResult(new File(args[1])));
+	}
+}
