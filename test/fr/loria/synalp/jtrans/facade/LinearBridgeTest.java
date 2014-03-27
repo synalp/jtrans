6c5c21b4ded4c7fe947f358317bf1098ad4def4d
Iliyas Jorio <inv.jorio@gmail.com>
2014-03-27 10:02:40 +0100

    nextSingle

--- test/fr/loria/synalp/jtrans/facade/LinearBridgeTest.java
+++ test/fr/loria/synalp/jtrans/facade/LinearBridgeTest.java
@@ -1,6 +1,7 @@
 package fr.loria.synalp.jtrans.facade;
 
 import fr.loria.synalp.jtrans.elements.Anchor;
+import fr.loria.synalp.jtrans.elements.Element;
 import fr.loria.synalp.jtrans.elements.Word;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -97,6 +98,78 @@ public class LinearBridgeTest {
 		Assert.assertEquals(trackB.elts.get(0), sl[1].getInitialAnchor());
 		Assert.assertEquals(trackB.elts.get(2), sl[1].getFinalAnchor());
 		Assert.assertEquals(trackB.elts.subList(1, 2), sl[1]);
+
+		Assert.assertFalse(bridge.hasNext());
+	}
+
+
+	private static boolean nullOrEmpty(AnchorSandwich s) {
+		return s == null || s.isEmpty();
+	}
+
+	@Test
+	public void testSimultaneous2() {
+		List<Track> trackList = new ArrayList<Track>();
+		Track trackA = new Track("A");
+		Track trackB = new Track("B");
+		trackList.add(trackA);
+		trackList.add(trackB);
+
+		{
+			final List<Element> e = trackA.elts;
+
+			e.add(Anchor.timedAnchor(0));
+			e.add(new Word("abc"));
+			e.add(Anchor.timedAnchor(1));
+
+			// B: "def"
+
+			e.add(Anchor.timedAnchor(2));
+			e.add(new Word("ghi"));
+			e.add(new Word("jkl"));
+			e.add(new Word("mno"));
+			e.add(Anchor.timedAnchor(3));
+		}
+
+		{
+			final List<Element> e = trackB.elts;
+
+			// A: "abc"
+
+			e.add(Anchor.timedAnchor(1));
+			e.add(new Word("def"));
+//			e.add(Anchor.timedAnchor(2));
+
+			e.add(Anchor.timedAnchor(2));
+			e.add(new Word("pqr"));
+			e.add(new Word("stu"));
+			e.add(Anchor.timedAnchor(3));
+
+//			e.add(Anchor.timedAnchor(3));
+			e.add(new Word("vwx"));
+			e.add(Anchor.timedAnchor(4));
+		}
+
+		LinearBridge bridge = new LinearBridge(trackList);
+		AnchorSandwich[] sl;
+
+		Assert.assertTrue(bridge.hasNext());
+
+		sl = bridge.next();
+		Assert.assertFalse(nullOrEmpty(sl[0]));
+		Assert.assertTrue(nullOrEmpty(sl[1]));
+
+		sl = bridge.next();
+		Assert.assertTrue(nullOrEmpty(sl[0]));
+		Assert.assertFalse(nullOrEmpty(sl[1]));
+
+		sl = bridge.next();
+		Assert.assertFalse(nullOrEmpty(sl[0]));
+		Assert.assertFalse(nullOrEmpty(sl[1]));
+
+		sl = bridge.next();
+		Assert.assertTrue(nullOrEmpty(sl[0]));
+		Assert.assertFalse(nullOrEmpty(sl[1]));
 
 		Assert.assertFalse(bridge.hasNext());
 	}
@@ -219,6 +292,74 @@ public class LinearBridgeTest {
 		Assert.assertEquals(Anchor.orderedTimelessAnchor(0),
 				wordSeq.getInitialAnchor());
 		Assert.assertNull(wordSeq.getFinalAnchor());
+	}
+
+
+	@Test
+	public void nextSingle() {
+		List<Track> trackList = new ArrayList<Track>();
+		Track trackA = new Track("A");
+		Track trackB = new Track("B");
+		trackList.add(trackA);
+		trackList.add(trackB);
+
+		{
+			final List<Element> e = trackA.elts;
+
+			e.add(Anchor.orderedTimelessAnchor(0));
+			e.add(new Word("abc"));
+			e.add(Anchor.orderedTimelessAnchor(1));
+
+			// B: "def"
+
+			e.add(Anchor.orderedTimelessAnchor(2));
+			e.add(new Word("ghi"));
+			e.add(new Word("jkl"));
+			e.add(new Word("mno"));
+			e.add(Anchor.orderedTimelessAnchor(3));
+		}
+
+		{
+			final List<Element> e = trackB.elts;
+
+			// A: "abc"
+
+			e.add(Anchor.orderedTimelessAnchor(1));
+			e.add(new Word("def"));
+//			e.add(Anchor.orderedTimelessAnchor(2));
+
+			e.add(Anchor.orderedTimelessAnchor(2));
+			e.add(new Word("pqr"));
+			e.add(new Word("stu"));
+//			e.add(Anchor.orderedTimelessAnchor(3));
+
+			e.add(Anchor.orderedTimelessAnchor(3));
+			e.add(new Word("vwx"));
+			e.add(Anchor.orderedTimelessAnchor(4));
+		}
+
+		LinearBridge lb = new LinearBridge(trackList);
+		AnchorSandwich s;
+
+		s = lb.nextSingle();
+		Assert.assertEquals(1, s.size());
+		Assert.assertEquals("abc", s.get(0).toString());
+
+		s = lb.nextSingle();
+		Assert.assertEquals(1, s.size());
+		Assert.assertEquals("def", s.get(0).toString());
+
+		s = lb.nextSingle();
+		Assert.assertEquals(3, s.size());
+		Assert.assertEquals("ghi", s.get(0).toString());
+		Assert.assertEquals("jkl", s.get(1).toString());
+		Assert.assertEquals("mno", s.get(2).toString());
+
+		s = lb.nextSingle();
+		Assert.assertEquals(1, s.size());
+		Assert.assertEquals("vwx", s.get(0).toString());
+
+		Assert.assertFalse(lb.hasNext());
 	}
 
 }
