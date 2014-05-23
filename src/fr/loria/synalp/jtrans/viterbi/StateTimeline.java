c901bc16a62b01d0ee35857416dbd0a5426c7943
Iliyas Jorio <inv.jorio@gmail.com>
2014-05-23 15:52:02 +0200

    StateTimeline transition manipulation

--- src/fr/loria/synalp/jtrans/viterbi/StateTimeline.java
+++ src/fr/loria/synalp/jtrans/viterbi/StateTimeline.java
@@ -5,6 +5,7 @@ import fr.loria.synalp.jtrans.elements.Word;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class StateTimeline {
 
@@ -44,6 +45,7 @@ public class StateTimeline {
 		segments = new ArrayList<>(other.segments);
 		uniqueWords = new ArrayList<>(other.uniqueWords);
 		frames = other.frames;
+		assert verify();
 	}
 
 
@@ -142,6 +144,94 @@ public class StateTimeline {
 
 			segStart += seg.length;
 		}
+	}
+
+
+
+	/**
+	 * Modifies the position of a transition between two segments.
+	 * @param lhsIdx index of the segment on the lefthand side of the transition
+	 * @param lhsNewLength new length to give to the LHS segment
+	 */
+	public void modifyTransition(int lhsIdx, int lhsNewLength) {
+		assert lhsIdx < segments.size()-1;
+		assert lhsNewLength > 0;
+		assert lhsNewLength <= getMaxLengthExpandingRightward(lhsIdx);
+
+		Segment a = segments.get(lhsIdx);
+		Segment b = segments.get(lhsIdx+1);
+
+		int delta = lhsNewLength - a.length;
+		a.length += delta;
+		b.length -= delta;
+
+		assert verify();
+	}
+
+
+	/**
+	 * Returns the maximum length for a segment expanding toward the right,
+	 * without erasing/overlapping any segments on its righthand side.
+	 * Important: the segment may still grow on its lefthand side!
+	 */
+	public int getMaxLengthExpandingRightward(int segIdx) {
+		assert segIdx < segments.size()-1;
+		Segment a = segments.get(segIdx);
+		Segment b = segments.get(segIdx+1);
+		return a.length + (b.length - 1);
+	}
+
+
+	public int transitionCount() {
+		return segments.size();
+	}
+
+
+	public boolean verify() {
+		int frameSum = 0;
+		for (Segment seg: segments) {
+			assert seg.length >= 1;
+			frameSum += seg.length;
+		}
+		assert frameSum == frames;
+		return true;
+	}
+
+
+	/**
+	 * Randomly changes the length of two adjacent segments.
+	 * @param maxDist The new lengths cannot stray further than this distance
+	 *                from the initial lengths. Use a negative number to bypass
+	 *                this limitation.
+	 */
+	public void wiggle(Random random, int maxDist) {
+		int lhsSeg = -1;
+		int maxLength = -1;
+
+		while (maxLength <= 1) {
+			lhsSeg = random.nextInt(transitionCount()-1);
+			maxLength = getMaxLengthExpandingRightward(lhsSeg);
+		}
+
+		int newLength;
+
+		if (maxDist < 0) {
+			newLength = 1 + random.nextInt(maxLength);
+		} else {
+			int currLength = segments.get(lhsSeg).length;
+
+			int min = Math.max(currLength - maxDist, 1);
+			int max = Math.min(currLength + maxDist, maxLength); // inclusive
+
+			newLength = currLength;
+			while (currLength == newLength) {
+				newLength = min + random.nextInt(1 + max - min);
+			}
+
+			assert maxDist >= Math.abs(newLength - currLength);
+		}
+
+		modifyTransition(lhsSeg, newLength);
 	}
 
 }
